dc79d8af96a1645b9a33b3d1c3d86c7f2e75b9e6
Tom Mahler <tom@mahler.tech>
2019-05-17 17:05:27 +0300

    split to train test and unused

---
+++ split_train_test.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 17 16:44:39 2019
+
+@author: Tom
+"""
+
+import shutil
+
+train_pairs = []
+train_names = set()
+with open("trainPairs.txt") as train:
+  train_files = train.readlines()[1:]
+  
+for pair in train_files:
+  pair = pair[:-1].split('\t')
+  if len(pair) == 3:
+    name1 = pair[0]
+    id1 = pair[1].zfill(4)
+    name2 = name1
+    id2 = pair[2].zfill(4)
+    
+    train_names.add(name1)
+    
+  elif len(pair) == 4:
+    name1 = pair[0]
+    id1 = pair[1].zfill(4)
+    name2 = pair[2]
+    id2 = pair[3].zfill(4)
+    
+    train_names.add(name1)
+    train_names.add(name2)
+  train_pairs.append((name1 + '_' + id1, name2 + '_' + id2))
+train_names = list(train_names)
+#print(train_files)
+
+test_pairs = []
+test_names = set()
+with open("testPairs.txt") as test:
+  test_files = test.readlines()[1:]
+  
+for pair in test_files:
+  pair = pair[:-1].split('\t')
+  if len(pair) == 3:
+    name1 = pair[0]
+    id1 = pair[1].zfill(4)
+    name2 = name1
+    id2 = pair[2].zfill(4)
+    
+    test_names.add(name1)
+    
+  elif len(pair) == 4:
+    name1 = pair[0]
+    id1 = pair[1].zfill(4)
+    name2 = pair[2]
+    id2 = pair[3].zfill(4)
+    
+    test_names.add(name1)
+
+    test_names.add(name2)
+  test_pairs.append((name1 + '_' + id1, name2 + '_' + id2))
+test_names = list(test_names)
+
+src = "lfw2/"
+train_dir = "data/train/"
+test_dir = "data/test/"
+
+for name in train_names:
+  print("Moving " + name + "...")
+  shutil.move(src + name, train_dir + name)
+
+for name in test_names:
+  print("Moving " + name + "...")
+  shutil.move(src + name, test_dir + name)
+  
