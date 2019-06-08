20734c961c3c2614cffc0441f7d9d98ac265aad0
Tom Mahler <tom@mahler.tech>
2019-06-08 14:41:37 +0300

    add contrastive_loss

--- distance.py
+++ distance.py
@@ -14,4 +14,11 @@ def distance_accuracy(y_true, y_pred, th=0.5, K=K):
     '''Compute classification accuracy with a fixed threshold on distances.
     '''
     return K.mean(K.equal(y_true, K.cast(y_pred < th, y_true.dtype)))
-  
+
+def contrastive_loss(y_true, y_pred, margin=0.5):
+    '''Contrastive loss from Hadsell-et-al.'06
+    http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
+    '''
+    square_pred = K.square(y_pred)
+    margin_square = K.square(K.maximum(margin - y_pred, 0))
+    return K.mean(y_true * square_pred + (1 - y_true) * margin_square)
